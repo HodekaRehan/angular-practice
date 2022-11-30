076101cd47447d6411819fe54a42dd2f0a0c001c
ReyHaan <hodekarehan@gmail.com>
2022-11-30 11:10:03 +0530

    directives complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  // numbers = [1, 2, 3, 4, 5]
+  onlyOdd: boolean =  false
+  oddNumbers = [1, 3, 5]
+  evenNumbers = [2, 4]
+  value = 5
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlight } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
+import { UnlessDirective } from './directives/unless/unless.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-    
+    BasicHighlight,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
