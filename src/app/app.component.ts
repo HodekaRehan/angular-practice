66aba7a22f306c965f791e20f8c86df47422abd3
ReyHaan <hodekarehan@gmail.com>
2022-11-30 10:04:56 +0530

    created new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  serverElements = [
-    {type: 'server', name: 'Test Server', content: 'Some Content' }
-  ]
-
-  onServerAdded(serverData: {srvName: string, srvContent: string}){
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.srvName,
-      content: serverData.srvContent
-    })
-  }
-
-  onBlueprintAdded(blueprintData: {srvName: string, srvContent: string}){
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.srvName,
-      content: blueprintData.srvContent
-    })
-  }
+  
 }
